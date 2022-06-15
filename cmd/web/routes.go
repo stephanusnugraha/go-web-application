5aedc8262625788dcd2df3b5f3b02bac74215f0f
Stephanus Nugraha <mail@stephanusnugraha.com>
2022-06-15 18:05:25 +0700

    fixing virtual-terminal

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -10,6 +10,9 @@ func (app *application) routes() http.Handler {
 	mux.Use(SessionLoad)
 
 	mux.Get("/virtual-terminal", app.VirtualTerminal)
+	mux.Post("/virtual-terminal-payment-succeeded", app.VirtualTerminalPaymentSucceeded)
+	mux.Get("/virtual-terminal-receipt", app.VirtualTerminalReceipt)
+
 	mux.Get("/", app.Home)
 	mux.Post("/payment-succeeded", app.PaymentSucceeded)
 	mux.Get("/receipt", app.Receipt)
