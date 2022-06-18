7bbb4e723617a298793f61b2531a633e0c15444f
Stephanus Nugraha <mail@stephanusnugraha.com>
2022-06-18 18:10:14 +0700

    add stubs for front-end; and setting up the form subscribe

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -280,3 +280,20 @@ func (app *application) ChargeOnce(w http.ResponseWriter, r *http.Request) {
 		app.errorLog.Println(err)
 	}
 }
+
+func (app *application) BronzePlan(w http.ResponseWriter, r *http.Request) {
+	widget, err := app.DB.GetWidget(2)
+	if err != nil {
+		app.errorLog.Println(err)
+		return
+	}
+
+	data := make(map[string]interface{})
+	data["widget"] = widget
+
+	if err := app.renderTemplate(w, r, "bronze-plan", &templateData{
+		Data: data,
+	}); err != nil {
+		app.errorLog.Println(err)
+	}
+}
