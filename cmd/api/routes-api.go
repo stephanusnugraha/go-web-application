a09a94994dfe4fdbe1952961cadd278a43c94a48
Stephanus Nugraha <mail@stephanusnugraha.com>
2022-06-19 18:47:09 +0700

    add route and handler for auth; add helpers (readJSON & badRequest)

--- cmd/api/routes-api.go
+++ cmd/api/routes-api.go
@@ -22,5 +22,7 @@ func (app application) routes() http.Handler {
 
 	mux.Post("/api/create-customer-and-subscribe-to-plan", app.CreateCustomerAndSubscribeToPlan)
 
+	mux.Post("/api/authenticate", app.CreateAuthToken)
+
 	return mux
 }
