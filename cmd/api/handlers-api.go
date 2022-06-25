2a708851a785079cb3f5633be253ab010380bbcb
Stephanus Nugraha <mail@stephanusnugraha.com>
2022-06-25 10:02:56 +0700

    validate the token on the back-end

--- cmd/api/handlers-api.go
+++ cmd/api/handlers-api.go
@@ -322,8 +322,12 @@ func (app *application) authenticateToken(r *http.Request) (*models.User, error)
 	}
 
 	// get the user from the tokens table
+	user, err := app.DB.GetUserForToken(token)
+	if err != nil {
+		return nil, errors.New("no matching user found")
+	}
 
-	return &u, nil
+	return user, nil
 }
 
 func (app *application) CheckAuthentication(w http.ResponseWriter, r *http.Request) {
