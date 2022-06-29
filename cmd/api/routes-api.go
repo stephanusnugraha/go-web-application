0bf87d9628f5f000edc91235af4e3db0b8821ef2
Stephanus Nugraha <mail@stephanusnugraha.com>
2022-06-29 17:03:43 +0700

    add sendmail function

--- cmd/api/routes-api.go
+++ cmd/api/routes-api.go
@@ -24,6 +24,7 @@ func (app application) routes() http.Handler {
 
 	mux.Post("/api/authenticate", app.CreateAuthToken)
 	mux.Post("/api/is-authenticated", app.CheckAuthentication)
+	mux.Post("/api/forgot-password", app.SendPasswordResetEmail)
 
 	mux.Route("/api/admin", func(r chi.Router) {
 		r.Use(app.Auth)
